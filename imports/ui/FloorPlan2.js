a948495fc343671bba460f9fbd23bc91c8c627e4
Janice Toh <janice.tohy2@gmail.com>
2021-04-09 02:04:40 +0800

    Change floorplan svg

--- imports/ui/FloorPlan2.js
+++ imports/ui/FloorPlan2.js
@@ -146,58 +146,134 @@ export class FloorPlan extends React.Component {
         return( 
             <div>
                 <h1>Floor Plan</h1>
-                <svg width="650" height="500" style={{border: '5px solid'}}>
-                    <g>
-                        <rect x="20" y="20" width="200" height="200" style={{fill:this.state.color0, cursor:'pointer', outline:'2px solid'}} onClick={this.handleRoom0}/>
-                        <text x="68" y="100" fontFamily="Verdana" fontSize="16" style={{cursor:'pointer'}} onClick={this.handleRoom0}>Upper Level</text>
-                        <text x="45" y="120" fontFamily="Verdana" fontSize="16" style={{cursor:'pointer'}} onClick={this.handleRoom0}>Residential Lounge</text>
-                        <text x="60" y="180" fontFamily="Verdana" fontSize="32" style={{cursor:'pointer'}} onClick={this.handleRoom0}>Room 0</text>
-                    </g>
-
-                    <g>
-                        <rect x="20" y="250" width="80" height="200" style={{fill:this.state.color1, cursor:'pointer', outline:'2px solid'}} onClick={this.handleRoom1}/>
-                        <text x="33" y="330" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom1}>SINGLE</text>
-                        <text x="24" y="350" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom1}>BEDROOM</text>
-                        <text x="38" y="410" fontFamily="Verdana" fontSize="32" style={{cursor:'pointer'}} onClick={this.handleRoom1}>R1</text>
-                    </g>
-
-                    <g>
-                        <rect x="120" y="250" width="80" height="200" style={{fill:this.state.color2, cursor:'pointer', outline:'2px solid'}} onClick={this.handleRoom2}/>
-                        <text x="133" y="330" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom2}>SINGLE</text>
-                        <text x="124" y="350" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom2}>BEDROOM</text>
-                        <text x="138" y="410" fontFamily="Verdana" fontSize="32" style={{cursor:'pointer'}} onClick={this.handleRoom2}>R2</text>
-                    </g>
-
-                    <g>
-                        <rect x="220" y="250" width="80" height="200" style={{fill:this.state.color3, cursor:'pointer', outline:'2px solid'}} onClick={this.handleRoom3}/>
-                        <text x="233" y="330" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom3}>SINGLE</text>
-                        <text x="224" y="350" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom3}>BEDROOM</text>
-                        <text x="238" y="410" fontFamily="Verdana" fontSize="32" style={{cursor:'pointer'}} onClick={this.handleRoom3}>R3</text>
-                    </g>
-
-                    <g>
-                        <rect x="320" y="250" width="80" height="200" style={{fill:this.state.color4, cursor:'pointer', outline:'2px solid'}} onClick={this.handleRoom4}/>
-                        <text x="333" y="330" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom4}>SINGLE</text>
-                        <text x="324" y="350" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom4}>BEDROOM</text>
-                        <text x="338" y="410" fontFamily="Verdana" fontSize="32" style={{cursor:'pointer'}} onClick={this.handleRoom4}>R4</text>
-                    </g>
-
-                    <g>
-                        <rect x="420" y="250" width="80" height="200" style={{fill:this.state.color5, cursor:'pointer', outline:'2px solid'}} onClick={this.handleRoom5}/>
-                        <text x="433" y="330" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom5}>SINGLE</text>
-                        <text x="424" y="350" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom5}>BEDROOM</text>
-                        <text x="438" y="410" fontFamily="Verdana" fontSize="32" style={{cursor:'pointer'}} onClick={this.handleRoom5}>R5</text>
-                    </g>
-
-                    <g>
-                        <rect x="520" y="250" width="80" height="200" style={{fill:this.state.color6, cursor:'pointer', outline:'2px solid'}} onClick={this.handleRoom6}/>
-                        <text x="533" y="330" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom6}>SINGLE</text>
-                        <text x="524" y="350" fontFamily="Verdana" fontSize="14" style={{cursor:'pointer'}} onClick={this.handleRoom6}>BEDROOM</text>
-                        <text x="538" y="410" fontFamily="Verdana" fontSize="32" style={{cursor:'pointer'}} onClick={this.handleRoom6}>R6</text>
-                    </g>
-                    
-                </svg>
-            </div>
+				<svg
+				   xmlns="http://www.w3.org/2000/svg"
+				   width="650" 
+				   height="500"
+				   viewBox="0 0 208 161"
+				   version="1.1">
+				  <g>
+					<g style={{cursor:'pointer'}}>
+						<rect style={{fill:this.state.color0}} width="80" height="70" x="7" y="7" 
+							onClick={this.handleRoom0}/>
+						<rect style={{fill:this.state.color1}} width="30" height="40" x="7" y="121" 
+							onClick={this.handleRoom1}/>
+						<rect style={{fill:this.state.color2}} width="30" height="40" x="40" y="121" 
+							onClick={this.handleRoom2}/>
+						<rect style={{fill:this.state.color3}} width="30" height="40" x="73" y="121" 
+							onClick={this.handleRoom3}/>
+						<rect style={{fill:this.state.color4}} width="30" height="40" x="106" y="121" 
+							onClick={this.handleRoom4}/>
+						<rect style={{fill:this.state.color5}} width="30" height="40" x="139" y="121" 
+							onClick={this.handleRoom5}/>
+						<rect style={{fill:this.state.color6}} width="30" height="40" x="172" y="121" 
+							onClick={this.handleRoom6}/>
+					</g>
+					<g style = {{fontSize:"4px", fontFamily:"sans-serif"}}>
+						<text x="30" y="30">UPPER LEVEL</text>
+						<text x="30" y="34">RESIDENTIAL</text>
+						<text x="30" y="38">LOUNGE</text>		
+						<text x="15" y="138">SINGLE</text>
+						<text x="12" y="142">BEDROOM</text>
+						<text x="48" y="138">SINGLE</text>
+						<text x="45" y="142">BEDROOM</text>	
+						<text x="81" y="138">SINGLE</text>
+						<text x="78" y="142">BEDROOM</text>
+						<text x="113" y="138">SINGLE</text>
+						<text x="110" y="142">BEDROOM</text>
+						<text x="146" y="138">SINGLE</text>
+						<text x="143" y="142">BEDROOM</text>		   
+						<text x="180" y="138">SINGLE</text>
+						<text x="176" y="142">BEDROOM</text>
+					</g>
+					<g style = {{fontSize:"10px", fontFamily:"Algerian"}}>
+						<text x="28" y="55">Room 0</text>
+						<text x="17" y="152">R1</text>
+						<text x="50" y="152">R2</text>
+						<text x="83" y="152">R3</text>
+						<text x="115" y="152">R4</text>
+						<text x="148" y="152">R5</text>
+						<text x="181" y="152">R6</text>
+					</g>
+					
+					<g style = {{fill:"none",stroke: "#4f4f4f", strokeWidth:"2"}}>
+						<path d="M 2,0 H 96 L 96,35 210,35" />
+						<path d="M 5,161 H 205"/>
+						
+						<g style = {{stroke:"#000000", strokeWidth:"1.5"}}>
+							<path d="M 87.5,7 H 8 7 v 55"/>
+							<path d="M 7,73 V 83 H 87 V 59"/>
+							<path d="M 87,49 V 41 h 8" />
+							<path d="m 20,83 v 13"/>
+							<path d="m 7,95 c 4,0 13,0 20,0"/>
+							<path d="m 51,83 0,11"/>
+							<path d="m 87,82 v 13" />
+							<path d="M 88,95 68,95"/>
+							<path d="m 43,95 c 3,0 10,0 15,0" />
+							<path d="m 144,41 -22,0 c 0,13 0,25 0,38 l 5,0"/>
+							<path d="m 160,41 14,0"/>
+							<path d="m 155,95 h 52"/>
+							<path d="m 184,41 24,0"/>
+							<path d="m 38.5,121 v 38.5"/>
+							<path d="m 71.5,121 v 38.5"/>
+							<path d="m 104.5,121 v 38.5"/>
+							<path d="m 137.5,121 v 38.5"/>
+							<path d="m 170.5,121 v 38.5"/>
+							<path d="m 7,161 v -40 H 202 v 40"/>
+						</g>
+						
+						<g style = {{stroke:"#000000", strokeWidth:"0.5"}}>
+							<path d="M 7,84 7,95"/>
+							<path d="m 27,95 17,0"/>
+							<path d="m 68,95 -10,0"/>
+							<path d="M 87,41 V 8"/>
+						</g>
+						
+						<g style = {{stroke:"#ffffff", strokeWidth:"1.5"}}>
+							<path d="m 21.5,121 11,0" />
+							<path d="m 122.5,121 10.5,-0"/>
+							<path d="m 56,121 10.5,-0" />
+							<path d="m 155,121 10.65,0"/>
+							<path d="m 76,121 10.5,0" />
+							<path d="M 175,121 185,121"/>
+						</g>
+						
+						<g style = {{stroke:"#000000", strokeWidth:"0.25"}}>
+							<path d="M 7,62 A 10.5,11 0 0 1 18,73" />
+							<path d="M 18,73 7,73 7,70" />
+							<path d="m 7,61 0,3"/>
+							<path d="m 87,-59 a 10,10 0 0 1 10,10" transform="scale(1,-1)" />
+							<path d="M 97,49 87,49 l 0,5"/>
+							<path d="m 87,48 0,3"/>
+							<path d="m 32,133 a 10,10 0 0 1 -8,-3 10,10 0 0 1 -3,-8" />
+							<path d="m 32,133 0,-12"/>	
+							<path d="m 21,122 10.5,-0"/>
+							<path d="m 21,120 10.5,0" />
+							<path d="m 133,133 a 10.5,10.5 0 0 1 -8,-3 10.5,10.5 0 0 1 -3,-8" />
+							<path d="m 133,133 0,-12" />
+							<path d="m 122,122 10.5,-0"/>
+							<path d="m 122,120 10.5,0" />
+							<path d="m 66.5,133 a 10.5,10.5 0 0 1 -8,-3 10.5,10.5 0 0 1 -3,-8" />
+							<path d="m 66.5,133 0,-12"/>
+							<path d="m 56,122 10.5,-0" />
+							<path d="m 56,120 10.5,0" />
+							<path d="m 166,133 a 10.5,10.5 0 0 1 -8,-3 10.5,10.5 0 0 1 -3,-8" />
+							<path d="m 166,133 0,-12"/>
+							<path d="m 155,122 10.5,0"/>
+							<path d="m 155,120 10.5,0"/>
+							<path d="m 87,122 a 12,10.5 0 0 1 -3,8 12,10.5 0 0 1 -8,4" />
+							<path d="m 76,134 0,-12"/>
+							<path d="m 76,122 10.5,-0"/>
+							<path d="m 76,122 10.5,0"/>
+							<path d="m 185,122 a 12,10.5 0 0 1 -3,8 12,10.5 0 0 1 -8,3" />
+							<path d="m 175,134 0,-12"/>
+							<path d="m 175,122 10.63618,-0"/>
+							<path d="m 175,120 10.5,0"/>
+						</g>
+					</g>
+				</g>
+			</svg>
+        </div>
         )
     }
 }
